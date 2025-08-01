4a67395e7b38a18de49931c6ed189aaeee9bd24a
Salvador Bravo <sa.bravo0901@gmail.com>
2025-07-31 23:16:01 -0400

    first commit: estructura inicial con backend

---
+++ backend/src/main/java/com/sbravoc/backend/BitacoraAcademicaApplication.java
@@ -0,0 +1,13 @@
+package com.sbravoc.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitacoraAcademicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitacoraAcademicaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sbravoc/backend/BitacoraAcademicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbravoc.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitacoraAcademicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
