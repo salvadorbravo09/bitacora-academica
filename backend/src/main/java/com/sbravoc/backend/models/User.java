36a62d7c510beafb329975188d35333d9063df1d
Salvador Bravo <sa.bravo0901@gmail.com>
2025-07-31 23:46:01 -0400

    feat: se agrego la entidad de User

---
+++ backend/src/main/java/com/sbravoc/backend/models/User.java
@@ -0,0 +1,35 @@
+package com.sbravoc.backend.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 6)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(nullable = false)
+    private boolean enabled;
+}
